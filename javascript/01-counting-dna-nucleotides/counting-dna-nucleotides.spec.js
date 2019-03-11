8dba2557867052da1df1ce4bd29e848e0e25e6d4
Piara <sharkattack85@gmail.com>
2019-03-11 08:03:09 -0700

    add 4 more tests to test suite

--- javascript/01-counting-dna-nucleotides/counting-dna-nucleotides.spec.js
+++ javascript/01-counting-dna-nucleotides/counting-dna-nucleotides.spec.js
@@ -8,4 +8,20 @@ describe('nucleotideCount()', () => {
   test('ACGT = 1 1 1 1', () => {
     expect(nucleotideCount('ACGT')).toEqual('1 1 1 1');
   });
+
+  test('AAAACCCCGGGGTTTT = 4 4 4 4', () => {
+    expect(nucleotideCount('AAAACCCCGGGGTTTT')).toEqual('4 4 4 4');
+  });
+
+  test('AGCTTCAGTCAGTA = 4 3 3 4', () => {
+    expect(nucleotideCount('AGCTTCAGTCAGTA')).toEqual('4 3 3 4');
+  });
+
+  test('GCTTTTCGATTAGGGCCATTACG = 4 5 6 8', () => {
+    expect(nucleotideCount('GCTTTTCGATTAGGGCCATTACG')).toEqual('4 5 6 8');
+  });
+
+  test('', () => {
+    expect(nucleotideCount()).toEqual('0 0 0 0');
+  });
 });
