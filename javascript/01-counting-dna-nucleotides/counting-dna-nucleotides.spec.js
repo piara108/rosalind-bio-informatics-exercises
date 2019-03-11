79c5fcb3173f4e28039164a79a73d0ce52d29a47
Piara <sharkattack85@gmail.com>
2019-03-11 00:30:32 -0700

    add second test to test sut suite

--- javascript/01-counting-dna-nucleotides/counting-dna-nucleotides.spec.js
+++ javascript/01-counting-dna-nucleotides/counting-dna-nucleotides.spec.js
@@ -1,7 +1,11 @@
 import { nucleotideCount } from './counting-dna-nucleotides';
 
 describe('nucleotideCount()', () => {
-  test('A', () => {
+  test('A = 1 0 0 0', () => {
     expect(nucleotideCount('A')).toEqual('1 0 0 0');
+  });
+
+  test('ACGT = 1 1 1 1', () => {
+    expect(nucleotideCount('ACGT')).toEqual('1 1 1 1');
   });
 });
