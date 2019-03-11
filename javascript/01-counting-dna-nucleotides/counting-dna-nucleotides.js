dd028d7c39fc47ac659ecd130f72704c2b78a703
Piara <sharkattack85@gmail.com>
2019-03-11 08:02:30 -0700

    error handling if no strand passed in

--- javascript/01-counting-dna-nucleotides/counting-dna-nucleotides.js
+++ javascript/01-counting-dna-nucleotides/counting-dna-nucleotides.js
@@ -1,8 +1,10 @@
 // Exercise Name:   Counting DNA Nucleotides
 // Exercise Number: 01
 
-export const nucleotideCount = (strandDNA) => {
+export const nucleotideCount = (strandDNA = undefined) => {
   let count = { 'A': 0, 'C': 0, 'G': 0, 'T': 0 };
+
+  if(strandDNA === undefined) { return '0 0 0 0'}
 
   strandDNA.split('').map(function(nt) { count[nt] += 1; });
 
