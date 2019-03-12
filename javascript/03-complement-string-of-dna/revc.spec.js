51301acd6c33f341bc9acc87b8ca1ceb701586e6
Piara <sharkattack85@gmail.com>
2019-03-11 21:32:38 -0700

    finish revc test suite

--- javascript/03-complement-string-of-dna/revc.spec.js
+++ javascript/03-complement-string-of-dna/revc.spec.js
@@ -1,7 +1,23 @@
-import { convertDNA() } from './revc';
+import { convertDNA } from './revc';
 
 describe('convertDNA()', () => {
   test('Empty', () => {
-    expect(convertDNA()).toEqual('');
+    expect(convertDNA('')).toEqual('');
+  });
+
+  test('AG', () => {
+    expect(convertDNA('AG')).toEqual('CT');
+  });
+
+  test('AAAACCCGGT', () => {
+    expect(convertDNA('AAAACCCGGT')).toEqual('ACCGGGTTTT');
+  });
+
+  test('AAACCCGGGTTT', () => {
+    expect(convertDNA('AAACCCGGGTTT')).toEqual('AAACCCGGGTTT');
+  });
+
+  test('ATGCCGTAATCGTAGCTAGGCC', () => {
+    expect(convertDNA('ATGCCGTAATCGTAGCTAGGCC')).toEqual('GGCCTAGCTACGATTACGGCAT');
   });
 });
