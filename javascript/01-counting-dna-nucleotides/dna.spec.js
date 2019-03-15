e924c1e4babd7ed4de5480201e2a314f26d422dd
Piara <sharkattack85@gmail.com>
2019-03-15 01:17:23 -0700

    changed description of each test

--- javascript/01-counting-dna-nucleotides/dna.spec.js
+++ javascript/01-counting-dna-nucleotides/dna.spec.js
@@ -1,23 +1,23 @@
 import { nucleotideCount } from './dna';
 
 describe('nucleotideCount()', () => {
-  test('A = 1 0 0 0', () => {
+  test('A', () => {
     expect(nucleotideCount('A')).toEqual('1 0 0 0');
   });
 
-  test('ACGT = 1 1 1 1', () => {
+  test('ACGT', () => {
     xexpect(nucleotideCount('ACGT')).toEqual('1 1 1 1');
   });
 
-  test('AAAACCCCGGGGTTTT = 4 4 4 4', () => {
+  test('AAAACCCCGGGGTTTT', () => {
     xexpect(nucleotideCount('AAAACCCCGGGGTTTT')).toEqual('4 4 4 4');
   });
 
-  test('AGCTTCAGTCAGTA = 4 3 3 4', () => {
+  test('AGCTTCAGTCAGTA', () => {
     xexpect(nucleotideCount('AGCTTCAGTCAGTA')).toEqual('4 3 3 4');
   });
 
-  test('GCTTTTCGATTAGGGCCATTACG = 4 5 6 8', () => {
+  test('GCTTTTCGATTAGGGCCATTACG', () => {
     xexpect(nucleotideCount('GCTTTTCGATTAGGGCCATTACG')).toEqual('4 5 6 8');
   });
 
