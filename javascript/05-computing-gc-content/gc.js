7d764a9f2662c61d623ba95a258f8bd71ff54393
piara108 <sharkattack85@gmail.com>
2019-03-20 08:27:13 -0700

    Wrote code to calculate GC ratio

--- javascript/05-computing-gc-content/gc.js
+++ javascript/05-computing-gc-content/gc.js
@@ -1,6 +1,20 @@
 // Exercise:  Computing GC Content
 // Number:    05
 
-export const computeGC = () => {
-  
+export const computeGC = (strand = undefined) => {
+  let countGC = 0;
+
+  strand.split('').map(function(nt) {
+    if(nt === 'G' || nt === 'C') {
+      countGC++;
+    }
+  });
+
+  let ratioGC = countGC / strand.length;
+
+  // console.log('GC Ratio: ' + countGC);
+
+  return ratioGC;
 }
+
+computeGC('ATGC');
