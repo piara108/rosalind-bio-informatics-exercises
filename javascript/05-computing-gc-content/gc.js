aa4961e763daeba58933c538c58ecb12b33e3cb4
piara108 <sharkattack85@gmail.com>
2019-03-20 20:37:38 -0700

    Add pseudocode to split apart FASTA formatted DNA string

--- javascript/05-computing-gc-content/gc.js
+++ javascript/05-computing-gc-content/gc.js
@@ -2,7 +2,13 @@
 // Number:    05
 
 export const computeGC = (strand = undefined) => {
+  let strandsDNA = [];
   let countGC = 0;
+  let dnaGC = {};
+
+  // Code here to separate FASTA formatted strings into individual DNA
+  // strands w/ correct tag
+
 
   strand.split('').map(function(nt) {
     if(nt === 'G' || nt === 'C') {
