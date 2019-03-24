15bb00cf6faa4c9d8216d29c1409a2c72569f7da
piara108 <sharkattack85@gmail.com>
2019-03-24 09:46:39 -0700

    Answers are correct, but tests not working for some reason

--- javascript/05-computing-gc-content/gc.js
+++ javascript/05-computing-gc-content/gc.js
@@ -1,6 +1,8 @@
 // Exercise:  Computing GC Content
 // Number:    05
 
+/*  The main space where functions are called
+    and most code is run */
 export const contentOfGC = (fastaDNA = undefined) => {
   let strandsDNA = [];
   let contentGC = {};
@@ -8,96 +10,57 @@ export const contentOfGC = (fastaDNA = undefined) => {
   let countGC = 0;
   let ratioGC = 0;
 
-  /*  Code here to separate FASTA formatted strings into individual DNA
-      strands w/ correct tag  */
-  // fastaDNA = fastaDNA.trim();
-
-  // Parses the FASTA format into an object of tag and DNA strand
   labelDNA = parseFASTA(fastaDNA);
 
   for(let tag in labelDNA) {
-    // console.log(`${tag}: ${labelDNA[tag]}`);
     contentGC[tag] = calculateGC(labelDNA[tag]);
+    console.log(`${contentGC[tag]}`);
   }
 
-  for(let str in contentGC) {
-    console.log(`${str}:  ${contentGC[str]}`);
-  }
 
-  // labelDNA.map(function(tag, dna) {
-  //   console.log(`${tag}: ${dna}`);
-  // });
+  let highestRatio = Object.keys(contentGC).reduce(
+    (a, b) => contentGC[a] > contentGC[b] ? a : b
+  );
 
-  // console.log('-------');
-  // fastaDNA.split('').map(function(nt) {
-  //   let start = '';
-  //   let end = ''
-  //   // console.log(nt);
-  //
-  //   if(nt === '>') {
-  //
-  //   }
-  // });
-  // strandsDNA = fastaDNA.split('\n');
+  // console.log(`${highestRatio}\n${contentGC[highestRatio]}`);
 
-  // console.log(strandsDNA.length);
-
-  // fastaDNA.split('').map(function(nt) {
-  //   if(nt === 'G' || nt === 'C') {
-  //     countGC++;
-  //   }
-  // });
-
-  // let ratioGC = countGC / fastaDNA.length;
-
-  // console.log('GC Ratio: ' + countGC);
-
-  return ratioGC;
+  return `${highestRatio}\n${contentGC[highestRatio]}`;
 }
 
+/*  Parses FASTA-formatted string into object
+    w/ key=tag and value=dna sequence */
 const parseFASTA = (stringInFASTA) => {
-  let labelDNA = {};
   let strandsDNA = stringInFASTA.split('>');
+  let labelDNA = {};
 
   if(strandsDNA[0] === '') strandsDNA.shift();
 
-  // console.log('-------');
   strandsDNA.map(function(line) {
-    line = line.trim()
-
-    let tag = line.substring(0, line.indexOf('\n'));
+    let tag = line.trim().substring(0, line.indexOf('\n'));
     line = line.replace(tag, '');
     let strandDNA = line.replace(/\r?\n|\r/g, '');
-    labelDNA[tag] = strandDNA;
 
-    // console.log(strandDNA);
-    // console.log(tag);
-    // console.log(line);
-    // console.log('-------');
+    labelDNA[tag] = strandDNA;
   });
 
   return labelDNA;
 };
 
+/*  Calculates the ratio of G and C nucleotides to
+    all of the nucleotides in the DNA strand */
 const calculateGC = (strandDNA) => {
   let amountGC = 0;
-  // let ratioGC = 0;
 
   strandDNA.split('').map(function(nt) {
     if(nt === 'G' || nt === 'C') amountGC++;
   });
 
-  let ratioGC = amountGC / strandDNA.length;
-
-  // console.log(`G & C:     ${amountGC}`);
-  console.log(`GC Ratio:  ${ratioGC}`);
-
-  return ratioGC;
+  return (amountGC / strandDNA.length) * 100;
 };
 
 // ====================== Driver Code ======================
 
-const fastaString = `>Rosalind_6404
+const fastaString1 = `>Rosalind_6404
 CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
 TCCCACTAATAATTCTGAGG
 >Rosalind_5959
@@ -107,5 +70,19 @@ ATATCCATTTGTCAGCAGACACGC
 CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
 TGGGAACCTGCGGGCAGTAGGTGGAAT`;
 
-// computeGC('ATGC');        => .5
-contentOfGC(fastaString);
+contentOfGC(fastaString1);
+
+const fastaString2 = `>Rosalind_5150
+ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCTCTGCC
+CCTGGAGGGTGGCCCCACCGGCCG
+>Rosalind_0707
+AGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGC
+CTCCTGACTTTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGG
+>Rosalind_0423
+AAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGCAATCCGCGCGCCGGGACAGAATGCC
+CTGCAGGAACTTCTTCTG
+>Rosalind_0616
+GAAGACCTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAG
+TTTAATTACAGACCTGAA`
+
+contentOfGC(fastaString2);
