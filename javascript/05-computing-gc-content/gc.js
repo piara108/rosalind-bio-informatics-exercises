1f1ff9cf79405f6d8d8aa0532e4251c8d3f246c9
piara108 <sharkattack85@gmail.com>
2019-03-25 22:42:01 -0700

    Add more driver code

--- javascript/05-computing-gc-content/gc.js
+++ javascript/05-computing-gc-content/gc.js
@@ -3,28 +3,24 @@
 
 /*  The main space where functions are called
     and most code is run */
-export const contentOfGC = (fastaDNA = undefined) => {
-  let strandsDNA = [];
-  let contentGC = {};
-  let labelDNA = {};
-  let countGC = 0;
-  let ratioGC = 0;
+export const contentOfGC = (fastaDNA) => {
+  let ratioGC = {};
+  // let labelDNA = {};
 
-  labelDNA = parseFASTA(fastaDNA);
+  let labelDNA = parseFASTA(fastaDNA);
 
   for(let tag in labelDNA) {
-    contentGC[tag] = calculateGC(labelDNA[tag]);
-    console.log(`${contentGC[tag]}`);
+    ratioGC[tag] = calculateGCRatio(labelDNA[tag]);
+    // console.log(`${tag}: ${ratioGC[tag]}`);
   }
 
-
-  let highestRatio = Object.keys(contentGC).reduce(
-    (a, b) => contentGC[a] > contentGC[b] ? a : b
+  let highestRatio = Object.keys(ratioGC).reduce(
+    (a, b) => ratioGC[a] > ratioGC[b] ? a : b
   );
 
-  // console.log(`${highestRatio}\n${contentGC[highestRatio]}`);
+  console.log(`${highestRatio}\n${ratioGC[highestRatio]}`);
 
-  return `${highestRatio}\n${contentGC[highestRatio]}`;
+  return `${highestRatio}\n${ratioGC[highestRatio]}`;
 }
 
 /*  Parses FASTA-formatted string into object
@@ -33,7 +29,8 @@ const parseFASTA = (stringInFASTA) => {
   let strandsDNA = stringInFASTA.split('>');
   let labelDNA = {};
 
-  if(strandsDNA[0] === '') strandsDNA.shift();
+
+  if(strandsDNA === '') strandsDNA.shift();
 
   strandsDNA.map(function(line) {
     let tag = line.trim().substring(0, line.indexOf('\n'));
@@ -43,12 +40,20 @@ const parseFASTA = (stringInFASTA) => {
     labelDNA[tag] = strandDNA;
   });
 
+  // strandsDNA.map(function(tag) {
+  //   if(tag === '') strandsDNA.shift();
+  //   console.log(tag);
+  // });
+
+  let objectLength = Object.getOwnPropertyNames(labelDNA);
+  console.log(`Object Size: ${objectLength.length}`);
+
   return labelDNA;
 };
 
 /*  Calculates the ratio of G and C nucleotides to
     all of the nucleotides in the DNA strand */
-const calculateGC = (strandDNA) => {
+const calculateGCRatio = (strandDNA) => {
   let amountGC = 0;
 
   strandDNA.split('').map(function(nt) {
@@ -60,29 +65,36 @@ const calculateGC = (strandDNA) => {
 
 // ====================== Driver Code ======================
 
-const fastaString1 = `>Rosalind_6404
-CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
-TCCCACTAATAATTCTGAGG
+const fastaTest1 = `>Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAA
+TAATTCTGAGG
 >Rosalind_5959
-CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
-ATATCCATTTGTCAGCAGACACGC
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATT
+TGTCAGCAGACACGC
 >Rosalind_0808
-CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
-TGGGAACCTGCGGGCAGTAGGTGGAAT`;
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCT
+GCGGGCAGTAGGTGGAAT`;
 
-contentOfGC(fastaString1);
+contentOfGC(fastaTest1);
 
-const fastaString2 = `>Rosalind_5150
-ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCTCTGCC
-CCTGGAGGGTGGCCCCACCGGCCG
+// console.log(contentOfGC(fastaTest1));
+
+// let dnaString = contentOfGC(fastaTest1);
+//
+// if(dnaString === 'Rosalind_0808\n60.91954022988506') {
+//   console.log('Yaaay, c\'est vrai!');
+// }
+
+const fastaTest2 = `>Rosalind_5150
+ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCTCTGC
+CCCTGGAGGGTGGCCCCACCGGCCG
 >Rosalind_0707
-AGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGC
-CTCCTGACTTTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGG
+AGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGCCTCCTGACTTTCCTCGCTTGGTG
+GTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGG
 >Rosalind_0423
-AAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGCAATCCGCGCGCCGGGACAGAATGCC
-CTGCAGGAACTTCTTCTG
+AAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGCAATCCGCGCGCCGGGACAGAATGC
+CCTGCAGGAACTTCTTCTG
 >Rosalind_0616
-GAAGACCTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAG
-TTTAATTACAGACCTGAA`
+GAAGACCTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAGTTTAATTACAGACCTGAA`;
 
-contentOfGC(fastaString2);
+// contentOfGC(fastaTest2);
