f58cc0c1b0fe67fc7558b22ec2f77d0c112492d8
piara108 <sharkattack85@gmail.com>
2019-03-28 07:35:30 -0700

    Converted code to an object

--- javascript/05-computing-gc-content/gc.js
+++ javascript/05-computing-gc-content/gc.js
@@ -1,66 +1,52 @@
 // Exercise:  Computing GC Content
 // Number:    05
 
-/*  The main space where functions are called
-    and most code is run */
-export const contentOfGC = (fastaDNA) => {
-  let ratioGC = {};
-  // let labelDNA = {};
-
-  let labelDNA = parseFASTA(fastaDNA);
-
-  for(let tag in labelDNA) {
-    ratioGC[tag] = calculateGCRatio(labelDNA[tag]);
-    // console.log(`${tag}: ${ratioGC[tag]}`);
+export class fastaDNA {
+  constructor(strandDNA) {
+    this.strandHashDNA = parseFASTAToHash(strandDNA);
   }
 
-  let highestRatio = Object.keys(ratioGC).reduce(
-    (a, b) => ratioGC[a] > ratioGC[b] ? a : b
-  );
+  contentOfGC(fastaDNA) {
+    let ratioGC = {};
 
-  console.log(`${highestRatio}\n${ratioGC[highestRatio]}`);
+    let labelDNA = parseFASTAToHash(fastaDNA);
 
-  return `${highestRatio}\n${ratioGC[highestRatio]}`;
+    for(let tag in labelDNA) {
+      ratioGC[tag] = calculateGCRatio(labelDNA[tag]);
+    }
+
+    let highestRatio = Object.keys(ratioGC).reduce(
+      (a, b) => ratioGC[a] > ratioGC[b] ? a : b
+    );
+
+    return `${highestRatio}\n${ratioGC[highestRatio]}`;
+  }
+
+  parseFASTAToHash(stringInFASTA) {
+    let strandsDNA = stringInFASTA.split('>');
+    let labelDNA = {};
+
+    strandsDNA.map(function(line) {
+      let tag = line.trim().substring(0, line.indexOf('\n'));
+      line = line.replace(tag, '');
+      let strandDNA = line.replace(/\r?\n|\r/g, '');
+
+      if(tag !== '') labelDNA[tag] = strandDNA.split('');
+    });
+
+    return labelDNA;
+  }
+
+  calculateGCRatio(strandDNArray) {
+    let amountGC = 0;
+
+    strandDNArray.map(function(nt) {
+      if(nt === 'G' || nt === 'C') amountGC += 1;
+    });
+
+    return (amountGC / strandDNArray.length) * 100;
+  }
 }
-
-/*  Parses FASTA-formatted string into object
-    w/ key=tag and value=dna sequence */
-const parseFASTA = (stringInFASTA) => {
-  let strandsDNA = stringInFASTA.split('>');
-  let labelDNA = {};
-
-  strandsDNA.map(function(line) {
-    let tag = line.trim().substring(0, line.indexOf('\n'));
-    line = line.replace(tag, '');
-    let strandDNA = line.replace(/\r?\n|\r/g, '');
-
-    if(tag !== '') labelDNA[tag] = strandDNA;
-
-  });
-
-  // console.log(labelDNA);
-
-  let objectLength = Object.getOwnPropertyNames(labelDNA);
-  // console.log(`Object Size: ${objectLength.length}`);
-
-  return labelDNA;
-};
-
-/*  Calculates the ratio of G and C nucleotides to
-    all of the nucleotides in the DNA strand */
-const calculateGCRatio = (strandDNA) => {
-  let amountGC = 0;
-
-  // console.log(strandDNA);
-  // console.log('====================================');
-  const strandLength = strandDNA.length;
-
-  strandDNA.split('').map(function(nt) {
-    if(nt === 'G' || nt === 'C') amountGC += 1;
-  });
-
-  return (amountGC / strandLength) * 100;
-};
 
 // ====================== Driver Code ======================
 
@@ -74,15 +60,9 @@ TGTCAGCAGACACGC
 CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCT
 GCGGGCAGTAGGTGGAAT`;
 
-contentOfGC(fastaTest1);
+let dnaRatio1 = fastaDNA
 
-// console.log(contentOfGC(fastaTest1));
-
-// let dnaString = contentOfGC(fastaTest1);
-// //
-// if(dnaString === 'Rosalind_0808\n60.91954022988506') {
-//   console.log('Yaaay, c\'est vrai!');
-// }
+console.log('================================');
 
 const fastaTest2 = `>Rosalind_5150
 ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCTCTGC
@@ -95,5 +75,3 @@ AAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGCAATCCGCGCGCCGGGACAGAATGC
 CCTGCAGGAACTTCTTCTG
 >Rosalind_0616
 GAAGACCTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAGTTTAATTACAGACCTGAA`;
-
-// contentOfGC(fastaTest2);
