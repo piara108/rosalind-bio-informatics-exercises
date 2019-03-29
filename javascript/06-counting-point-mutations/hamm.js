c1e912117b483e8c780d988a3f4ef5914879c606
piara108 <sharkattack85@gmail.com>
2019-03-28 18:51:30 -0700

    Cleaned up code, solved problem

--- javascript/06-counting-point-mutations/hamm.js
+++ javascript/06-counting-point-mutations/hamm.js
@@ -4,14 +4,17 @@
 export const distanceHamming = (strandDNA1, strandDNA2) => {
   let dist = 0;
 
+  if(!(strandDNA1.length === strandDNA2.length)) {
+    throw new Error('Strands must be equal in length');
+  }
+
+  if(strandDNA1.length === 0 || strandDNA2.length === 0) {
+    throw new Error('Strands must be greater than 0 in length');
+  }
+
   for(var x = 0; x < strandDNA1.length; x++) {
     if(strandDNA1[x] !== strandDNA2[x]) dist++;
   }
 
-  return dist++
+  return dist++;
 };
-
-const strandDNA1 = 'GAGCCTACTAACGGGAT';
-const strandDNA2 = 'CATCGTAATGACGGCCT';
-
-console.log(distanceHamming(strandDNA1, strandDNA2));
