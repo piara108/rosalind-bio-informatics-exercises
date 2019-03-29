eff6cb6498d558d5c9c7d8981ffb7796a9b366d0
piara108 <sharkattack85@gmail.com>
2019-03-28 18:52:49 -0700

    Continued cndnl branches

--- javascript/06-counting-point-mutations/hamm.js
+++ javascript/06-counting-point-mutations/hamm.js
@@ -7,13 +7,13 @@ export const distanceHamming = (strandDNA1, strandDNA2) => {
   if(!(strandDNA1.length === strandDNA2.length)) {
     throw new Error('Strands must be equal in length');
   }
-
-  if(strandDNA1.length === 0 || strandDNA2.length === 0) {
+  else if(strandDNA1.length === 0 || strandDNA2.length === 0) {
     throw new Error('Strands must be greater than 0 in length');
   }
-
-  for(var x = 0; x < strandDNA1.length; x++) {
-    if(strandDNA1[x] !== strandDNA2[x]) dist++;
+  else {
+    for(var x = 0; x < strandDNA1.length; x++) {
+      if(strandDNA1[x] !== strandDNA2[x]) dist++;
+    }
   }
 
   return dist++;
