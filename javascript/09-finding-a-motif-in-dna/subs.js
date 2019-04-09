a06f1db806a244648b91843f8ed7966f85377365
piara108 <sharkattack85@gmail.com>
2019-04-09 07:37:26 -0700

    Deleted drive code, solved exercise 09

--- javascript/09-finding-a-motif-in-dna/subs.js
+++ javascript/09-finding-a-motif-in-dna/subs.js
@@ -2,7 +2,23 @@
 // Exercise Number: 09
 
 export const substringFinder = (t, s) => {
-  const substringLength = t.length;
+  const substrandLength = t.length;
+  const strandLength = s.length;
+  let locations = [];
 
-  
+  if(substrandLength === 0 || strandLength === 0) {
+    throw new Error('Substrand or Strand length must be greater than 0 bp');
+  }
+  else if(substrandLength >= strandLength) {
+    throw new Error('Substrand(t) must be shorter than Strand(s)')
+  }
+
+  for(var x = 0; x < strandLength; x++) {
+    var strandSlice = s.slice(x, x + substrandLength);
+    var index = x + 1;
+
+    if(t === strandSlice) { locations.push(index); }
+  }
+
+  return (locations.length > 0) ? locations : 'No motives found';
 };
