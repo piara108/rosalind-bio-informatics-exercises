6855cc19ec9a21fc32d7fe58e63a584c0230ce75
piara108 <sharkattack85@gmail.com>
2019-04-08 18:10:49 -0700

    Created new test, finished suit

--- javascript/08-translating-rna-into-protein/prot.spec.js
+++ javascript/08-translating-rna-into-protein/prot.spec.js
@@ -1,14 +1,31 @@
 import { translateRNA } from './prot';
 
 describe('Translating RNA into Protein', () => {
-  const strand1 = 'GCGAACGCACUC';
-  const strand2 = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA';
+  const warning = 'Must contain at least 3 nucleotides.';
+
+  const strand1 = '';
+  const strand2 = 'AC';
+  const strand3 = 'CCC';
+  const strand4 = 'AAAAAGAAA';
+  const strand5 = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA';
 
   test(`${strand1}`, () => {
-    expect(translateRNA(strand1)).toEqual('ANAL');
+    expect(() => translateRNA(strand1)).toThrow(new Error(warning));
   });
 
   test(`${strand2}`, () => {
-    expect(translateRNA(strand2)).toEqual('MAMAPRTEINSTRING');
+    expect(() => translateRNA(strand2)).toThrow(new Error(warning));
+  });
+
+  test(`${strand3}`, () => {
+    expect(translateRNA(strand3)).toEqual('P');
+  });
+
+  test(`${strand4}`, () => {
+    expect(translateRNA(strand4)).toEqual('KKK');
+  });
+
+  test(`${strand5}`, () => {
+    expect(translateRNA(strand5)).toEqual('MAMAPRTEINSTRING');
   });
 });
